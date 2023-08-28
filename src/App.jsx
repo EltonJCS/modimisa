30200e5f338c54f5cfd66df719e9a5cafab35edb
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-08-28 17:00:39 -0300

    Added home intro toggle animation and other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Canvas from "./canvas";
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="head-text">Modimisa</h1>
-      </div>
+      <main className="app transition-all ease-in">
+        <Home />
+        <Canvas />
+        <Customizer />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import {
+  slideAnimation,
+  headTextAnimation,
+  headContentAnimation,
+  headContainerAnimation,
+} from "../config/motion";
+
+import { CustomButton } from "../components";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-container"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                A SUA <br className="xl:block hidden" /> CAMISA.
+              </h1>
+            </motion.div>
+            <motion.div
+              className="flex flex-col gap-5"
+              {...headContentAnimation}
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Crie a sua, única e exclusiva, camisa com uma ferramenta de
+                customização 3D. <strong>Libere sua imaginação</strong>
+                {""} e defina seu próprio estilo.
+              </p>
+              <CustomButton
+                type="filled"
+                title="Customizar"
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                handleClick={() => (state.intro = false)}
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
